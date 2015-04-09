4321e9ec292ace468740a537f39ef421b0c9c741
root <root@localhost.localdomain>
2015-04-09 23:00:04 +0200

    Auto Synch

--- sync.sh
+++ sync.sh
@@ -26,7 +26,7 @@ tosync=("${tosync[@]}" "$HOME/.config/compton.conf@@@$P/compton.conf")
 
 #bin
 P="$D/bin"
-tosync=("${tosync[@]}" "$HOME/bin/tablette@@@$P/tablette" "$HOME/bin/youtubedl@@@$P/youtubedl" "$HOME/bin/dockerClean@@@$P/dockerClean")
+tosync=("${tosync[@]}" "$HOME/bin/tablette@@@$P/tablette" "$HOME/bin/youtubedl@@@$P/youtubedl" "$HOME/bin/dockerClean@@@$P/dockerClean" "$HOME/bin/dockerStop@@@$P/dockerStop")
 
 #maintenance
 P="$D/maintenance"
